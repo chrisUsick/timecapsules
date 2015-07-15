ac3d5aea1c764ac409f5410b40709e89a6dff88b
chrisUsick <christopher.usick@gmail.com>
2015-07-14 21:16:43 -0500

    adding home page basics

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def home
+    @capsules = current_user.capsules.where("sendDate <= ?", Date.today().next_month)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount Crono::Web, at: '/crono'
   get 'welcome/index'
+  get 'welcome/home'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
